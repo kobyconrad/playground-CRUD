3f0fa430d3a607294d8d7b1ae1ac16e997055e63
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-11 22:31:30 -0700

    added nodemon, form, and POST server side handler

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log("may the node be with you");
 });
 
-app.get("/", function (req, res) {
-  res.send("you tickled my get request");
-  res.end();
+app.get("/", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log("these are not the droids you are looking for");
 });
