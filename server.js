5170e1ac7edaafa6260196ec603792e547d6a90f
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-11 22:59:18 -0700

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 app.listen(3000, () => {
   console.log("may the node be with you");
 });
+
+MongoClient.connect(
+  "mongodb+srv://koby:grassphone2@cluster0-q6mwy.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    console.log("connected to database");
+  })
+  .catch((error) => console.error(error));
 
 // This is my body parser to read form data
 app.use(bodyParser.urlencoded({ extended: true }));
