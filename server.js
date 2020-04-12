c22a3b006523cb67ef4194738c6ebfdb4f2e40d7
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-11 22:37:02 -0700

    added body parser to read form data

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.listen(3000, () => {
   console.log("may the node be with you");
 });
 
+// This is my body parser to read form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// These are my handlers
 app.get("/", (req, res) => {
   console.log(__dirname);
   res.sendFile(__dirname + "/index.html");
@@ -12,4 +17,5 @@ app.get("/", (req, res) => {
 
 app.post("/quotes", (req, res) => {
   console.log("these are not the droids you are looking for");
+  console.log(req.body);
 });
