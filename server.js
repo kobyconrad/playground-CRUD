965d7c75793a6da029693c9c718cd64d94a01d17
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-12 20:26:21 -0700

    READING data from database

--- server.js
+++ server.js
@@ -7,25 +7,54 @@ app.listen(3000, () => {
   console.log("may the node be with you");
 });
 
+// This is my Mongo client which is going to wrap all of my express CRUD functionality
 MongoClient.connect(
   "mongodb+srv://koby:grassphone2@cluster0-q6mwy.mongodb.net/test?retryWrites=true&w=majority",
   { useUnifiedTopology: true }
 )
   .then((client) => {
     console.log("connected to database");
+    // I think this renames/sets up the database from test to SWQ
+    const db = client.db("star-wars-quotes");
+
+    // this creates a "collection" which we will use to POST data to
+    const quotesCollection = db.collection("quotes");
+
+    // This is my body parser to read form data
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // These are my handlers
+    app.get("/", (req, res) => {
+      console.log(__dirname);
+      res.sendFile(__dirname + "/index.html");
+      const cursor = db
+        .collection("quotes")
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+        })
+        .catch((error) => console.error(error));
+    });
+
+    // app.get("/", (req, res) => {
+    //   const cursor = db
+    //     .collection("quotes")
+    //     .find()
+    //     .toArray()
+    //     .then((results) => {
+    //       console.log(results);
+    //     })
+    //     .catch((error) => console.error(error));
+    // });
+
+    app.post("/quotes", (req, res) => {
+      //   console.log("these are not the droids you are looking for");
+      //   console.log(req.body);
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => res.redirect("/"))
+        .catch((error) => console.error(error));
+    });
   })
   .catch((error) => console.error(error));
-
-// This is my body parser to read form data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// These are my handlers
-app.get("/", (req, res) => {
-  console.log(__dirname);
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/quotes", (req, res) => {
-  console.log("these are not the droids you are looking for");
-  console.log(req.body);
-});
